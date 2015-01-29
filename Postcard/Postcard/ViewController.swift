53289a6a367b516ca257e53cedb6ce7046b16491
bdorner <bdorner@gmail.com>
2015-01-28 19:31:31 -0700

    first checking of challenge 1. can't get keyboard to popup after first time clicking on send message button. Also can't change button text back to send message from mail sent

---
+++ Postcard/Postcard/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Bradley Dorner on 1/27/15.
+//  Copyright (c) 2015 Bradley Dorner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func sendMailButton(sender: AnyObject) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+    
+    @IBAction func sendMailButtonInactive(sender: AnyObject) {
+        mailButton.setTitle("Send Message", forState: UIControlState.Normal)
+    }
+    
+}
+
